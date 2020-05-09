7a180fdad74dbe6ea7c2ce055dcb25786aefd992
MrDanielYew <65059587+MrDanielYew@users.noreply.github.com>
2020-05-09 21:40:43 +0800

    First Update of 2 files
    Start update of 2 files

---
+++ arithmetic.php
@@ -0,0 +1,20 @@
+<?php
+//arithmetic php
+//Perform arithmetic operations abd assign the results to variables.
+
+$addition = 2 + 4 ;
+$substraction = 6 -2 ;
+$multiplication = 5 * 3;
+$division = 15/3;
+$modulus = 5% 2;
+
+//Output the result
+echo "Perform addition: 2 + 4 = ".$addition."</br>";
+echo "Perform substraction: 6 - 2 = ".$substraction."</br>" ;
+echo "Perform multiplcation: 5 * 3 = ".$multiplication."</br>" ;
+echo "Perform division: 15 / 3 = ".$division."</br>" ;
+echo "Perform modulus: 5 % 2 = ".$modulus."</br>" ;
+
+
+
+?>
---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+//arithmetic php
+//Perform arithmetic operations abd assign the results to variables.
+echo "test1";
+$d=date("H:i:s");
+echo $d;
+
+//Updated on 8/5/20 with changes
+
+?>
